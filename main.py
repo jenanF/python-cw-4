0db901744c2475c1abb7eb8b83740d8deef1b633
jenanF <108689302+jenanF@users.noreply.github.com>
2023-08-29 23:47:33 +0300

    done

---
+++ main.py
@@ -0,0 +1,23 @@
+
+def my_name():
+    name = "jenan"
+    print("My name is", name)
+
+my_name()
+
+def my_meal(food, drink):
+    print(f"I like to eat {food} and while drinking {drink}")
+
+my_meal('cookies', "milk")
+
+def cube(number):
+    return number*number*number
+
+print(cube(9))
+
+def by_three(number):
+    if number %3 == 0:
+        return cube(number)
+    else:
+        return False
+print(by_three(4))
